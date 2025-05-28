ccf119caf05cd3f7091f550dbc0260099356844c
Antoni30 <artoapanta3@espe.edu.ec>
2025-05-28 03:44:28 -0500

    update

--- Sistema-Gestion-Notas-Liceo-La-Siembra-pancho/publicaciones/src/main/java/edu/espe/publicaciones/respository/AutorRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.espe.publicaciones.respository;
-
-import edu.espe.publicaciones.entity.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorRepository extends JpaRepository<Autor, Long> {
-}
--- Sistema-Gestion-Notas-Liceo-La-Siembra-pancho/publicaciones/src/main/java/edu/espe/publicaciones/respository/LibroRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.espe.publicaciones.respository;
-
-import edu.espe.publicaciones.entity.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepository extends JpaRepository<Libro, Integer> {
-}
